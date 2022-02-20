9ee2dd1631b8908b3813ca77a5262e343462b6b1
Radoncode <relbouni69@gmail.com>
2022-02-20 12:52:14 +0000

    hoc

---
+++ src/Goku.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import goku from './goku.png';
+import countHits from './countHits';
+
+
+class Goku extends Component {
+    
+    render() {
+
+        const {name, addOneHit, hocState, life} = this.props;
+
+        const lifeValue = life > 0 ? (<td>{life}%</td>)
+         : (<td><span className="badge bg-danger">Mort</span></td>);
+
+        const button = life > 0 ? (<button onClick={addOneHit}className="btn btn-success m-3">{name} Frappe</button>)
+         : (<button className="btn btn-danger m-3 disabled">Mort</button>);
+
+        return (
+            <div className="col">
+                <img src={goku} alt="goku" /><br />
+                {button}
+
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scpe="col">Coups</th>
+                            <th scpe="col">Vie</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{hocState.hits}</td>
+                            {lifeValue}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+        );
+    }
+}
+
+export default countHits(Goku, 20);
---
+++ src/Vegeta.js
@@ -0,0 +1,44 @@
+import { Component } from "react";
+import vegeta from './vegeta.png';
+import countHits from './countHits';
+
+
+class Vegeta extends Component {
+    
+    render() {
+        console.log(this.props);
+
+        const {name, addOneHit, hocState, life} = this.props;
+
+        const lifeValue = life > 0 ? (<td>{life}%</td>)
+         : (<td><span className="badge bg-danger">Mort</span></td>);
+
+        const button = life > 0 ? (<button onClick={addOneHit}className="btn btn-success m-3">{name} Frappe</button>)
+         : (<button className="btn btn-danger m-3 disabled">Mort</button>);
+
+        return (
+            <div className="col">
+                <img src={vegeta} alt="vegeta" /><br />
+                {button}
+
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scpe="col">Coups</th>
+                            <th scpe="col">Vie</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{hocState.hits}</td>
+                            {lifeValue}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+        );
+    }
+}
+
+export default countHits(Vegeta, 10);
